4ae81a351c64aa424bed8f04d80a59c790c0886c
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-22 21:46:20 -0600

    SDfds

--- breakfast.php
+++ breakfast.php
@@ -104,54 +104,42 @@
 			<div class="row">
 				<div class="col-md-4 col-sm-6">
 					<div class="feature-center animate-box" data-animate-effect="fadeIn">
-						<span class="icon">
-							<i class="ti-face-smile"></i>
-						</span>
+						
 						<h3>Happy People</h3>
 						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
 					<div class="feature-center animate-box" data-animate-effect="fadeIn">
-						<span class="icon">
-							<i class="ti-thought"></i>
-						</span>
+						
 						<h3>Creative Culinary</h3>
 						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
 					<div class="feature-center animate-box" data-animate-effect="fadeIn">
-						<span class="icon">
-							<i class="ti-truck"></i>
-						</span>
+						
 						<h3>Food Delivery</h3>
 						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
 					<div class="feature-center animate-box" data-animate-effect="fadeIn">
-						<span class="icon">
-							<i class="ti-face-smile"></i>
-						</span>
+						
 						<h3>Happy People</h3>
 						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
 					<div class="feature-center animate-box" data-animate-effect="fadeIn">
-						<span class="icon">
-							<i class="ti-thought"></i>
-						</span>
+						
 						<h3>Creative Culinary</h3>
 						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
 					<div class="feature-center animate-box" data-animate-effect="fadeIn">
-						<span class="icon">
-							<i class="ti-truck"></i>
-						</span>
+						
 						<h3>Food Delivery</h3>
 						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
 					</div>
