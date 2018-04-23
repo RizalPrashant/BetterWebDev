0f78fb3ee3ce012076b3fa22bd811974095a485a
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-23 13:28:05 -0600

    SDf

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 			
 			<div class="row">
 				<div class="col-sm-4 col-xs-12">
-					<div id="gtco-logo"><a href="index.html">Home</a></div>
+					<div id="gtco-logo"><a href="index.html">Home</a>  Hi, Prashant</div>
 				</div>
 				<div class="col-xs-8 text-right menu-1">
 					<ul>
