0479d4d85b68db9c0a31fbb8ff60710963ad1dd8
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-22 21:49:32 -0600

    SDfds

--- breakfast.php
+++ breakfast.php
@@ -97,9 +97,8 @@
 		<div class="gtco-container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
-					<h2 class="cursive-font">Our Services</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
+					<h2 class="cursive-font">Ingredients</h2>
+					
 			</div>
 			<div class="row">
 				<div class="col-md-4 col-sm-6">
