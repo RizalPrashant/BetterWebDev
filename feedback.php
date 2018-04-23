a665be40b29ff4595645d114374fe0d9affc810f
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-22 22:57:34 -0600

    ADSf

--- feedback.php
+++ feedback.php
@@ -3,12 +3,8 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Savory &mdash; Free Website Template, Free HTML5 Template by GetTemplates.co</title>
+	<title>Feedback</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-
-  
-
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 	
