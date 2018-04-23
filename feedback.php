a1e2f60b44b81064577564b3c2f0c661852b2f65
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-22 22:40:10 -0600

    ADSf

--- feedback.php
+++ feedback.php
@@ -117,29 +117,16 @@
 						<div class="row form-group">
 							<div class="col-md-12">
 								<label class="sr-only" for="message">Message</label>
-								<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Write us something"></textarea>
+								<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Write feedback"></textarea>
 							</div>
 						</div>
 						<div class="form-group">
-							<input type="submit" value="Send Message" class="btn btn-primary">
+							<input type="submit" value="Submit" class="btn btn-primary">
 						</div>
 
 					</form>		
 				</div>
-				<div class="col-md-5 col-md-push-1 animate-box">
-					
-					<div class="gtco-contact-info">
-						<h3>Contact Information</h3>
-						<ul>
-							<li class="address">198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-							<li class="phone"><a href="tel://1234567920">+ 1235 2355 98</a></li>
-							<li class="email"><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
-							<li class="url"><a href="http://FreeHTML5.co">FreeHTML5.co</a></li>
-						</ul>
-					</div>
-
-
-				</div>
+				
 				</div>
 			</div>
 		</div>
