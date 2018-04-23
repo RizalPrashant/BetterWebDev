43c3fbf5760b5f20e34c7d403d9c74c5a3d90e0f
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-22 21:19:51 -0600

    SDfds

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 				<div class="col-xs-8 text-right menu-1">
 					<ul>
 						<li class="has-dropdown">
-							Selections
+							<a href="#">Selections</a>
 							<ul class="dropdown">
 								<li><a href="#">Snacks</a></li>
 								<li><a href="#">Lunch</a></li>
