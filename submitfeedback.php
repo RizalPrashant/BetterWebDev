58e9a7cc4ca965c782822dba332e0d1f8c372640
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-23 16:29:55 -0600

    SDf

--- submitfeedback.php
+++ submitfeedback.php
@@ -5,12 +5,16 @@ $email = mysqli_real_escape_string($connection,$_GET['email']);
 $feedback = mysqli_real_escape_string($connection,$_GET['feedback']);
 $my_query = "";
 echo "<br>";
+echo $name;
+echo $email;
+echo $feedback;
 
 $my_query = "INSERT INTO Feedback(Name, Email, suggestion) VALUES ('$name','$email','$feedback')";
     echo "<br>";
 $result= mysqli_query($connection, $my_query);
     if($result){
-        header("Location:displayfeedback.php");
+        echo "Thank you for the feedback";
+        //header("Location:displayfeedback.php");
     
     }
     else{
