a3e3b2d96266c4089c562509240045594625784e
Prashant Rizal <Prashant@prashants-mbp.boisestate.edu>
2018-04-22 22:02:31 -0600

    SDfds

--- breakfast.php
+++ breakfast.php
@@ -198,27 +198,18 @@ Enjoy!</p>
 				
 				<div class="col-md-12 text-center">
 					<div class="gtco-widget">
-						<h3>Get In Touch</h3>
+						<h3>My Contact Information</h3>
 						<ul class="gtco-quick-contact">
-							<li><a href="#"><i class="icon-phone"></i> +1 234 567 890</a></li>
-							<li><a href="#"><i class="icon-mail2"></i> info@GetTemplates.co</a></li>
-							<li><a href="#"><i class="icon-chat"></i> Live Chat</a></li>
+							<li><i class="icon-phone"></i> +507-351-5303</li>
+							<li><i class="icon-mail2"></i> prsnt.rizal@gmail.com</li>
 						</ul>
 					</div>
-					<div class="gtco-widget">
-						<h3>Get Social</h3>
-						<ul class="gtco-social-icons">
-							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
-							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble"></i></a></li>
-						</ul>
-					</div>
+					
 				</div>
 
 				<div class="col-md-12 text-center copyright">
-					<p><small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-						<small class="block">Designed by <a href="http://gettemplates.co/" target="_blank">GetTemplates.co</a> Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></small></p>
+					<p><small class="block">&copy; Prashant Rizal</small> 
+						</p>
 				</div>
 
 			</div>
